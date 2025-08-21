da553896467d93710ce9a6ddcd82066b3298578c
Caramel-nerd <bismay80@gmail.com>
2025-08-22 04:49:40 +0530

    banking

--- Java/Banking.java
+++ Java/Banking.java
@@ -2,10 +2,11 @@ import java.util.Scanner;
 
 public class Banking {
 
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
         System.out.println("BANKING PORTAL");
-        Scanner scanner = new Scanner(System.in);
 
         int option;
         boolean isrun = true;
@@ -22,26 +23,50 @@ public class Banking {
             switch (option) {
 
                 case 1 -> Balance(amount);
-//                case 2 -> Withdraw();
-//                case 3 -> Deposit();
+                case 2 -> amount=Withdraw(amount);
+                case 3 -> amount=Deposit(amount);
                 case 4 -> isrun = false;
                 default -> System.out.println("50% charges for invalid option _|_");
             }
-
-            scanner.close();
         }
 
-
+        scanner.close();
     }
 
     static void Balance(double amount){
 
-        System.out.println("Balance");
+        System.out.printf("Balance : %f \n", amount);
     }
 
-    //desposit
+   static double Deposit(double amount){
+        double add;
+        System.out.println("Enter Amount :");
+        add = scanner.nextDouble();
 
-    //wothrae
+        if(add>0){
+        amount+=add;
+        }else {
+            System.out.print("Get money prick");
+        }
+        return amount;
+
+   }
+
+  static Double Withdraw(double amount){
+
+      double wid;
+      System.out.println("Enter Amount :");
+      wid = scanner.nextDouble();
+      if(wid<=amount && amount > 0) {
+          amount -= wid;
+      }
+      else {
+          System.out.println("BROKE ASS HAHA");
+      }
+      return amount;
+  }
+
+
 
 
 }
