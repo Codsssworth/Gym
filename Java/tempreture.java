954ad9ae5369cfc9e4c7155e377be18cddd8d2bb
Caramel-nerd <bismay80@gmail.com>
2025-08-22 03:52:47 +0530

    tempreture converter

--- Java/tempreture.java
+++ Java/tempreture.java
@@ -5,17 +5,25 @@ public class tempreture {
     public static void main(String[] args) {
 
        double temp,result;
-       String type;
+       String type ;
        Scanner scanner = new Scanner(System.in);
 
        System.out.print("Enter Tempreture :");
        temp = scanner.nextDouble();
+
+       while(temp!=-1){
 
        System.out.print("convert to Celsius or Farenhiet :");
        type = scanner.next().toUpperCase();
 
        result = (type.equals("C"))? (temp - 32)*5/9 : (temp * 1.8)+32;
 
-       System.out.printf("Converted temp = %f %s",result,type);
+       System.out.printf("Converted temp = %f %s \n",result,type);
+
+       System.out.print("Enter Tempreture (-1 to quit):");
+       temp = scanner.nextDouble();
+       }
+
+       scanner.close();
     }
 }
